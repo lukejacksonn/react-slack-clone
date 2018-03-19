9f4924b44c78e0f7b9870ab8817f77439bda3923
Luke Jackson <lukejacksonn@gmail.com>
2018-03-19 10:47:42 +0000

    Refactors UserHeader component

--- src/components/UserHeader/index.js
+++ src/components/UserHeader/index.js
@@ -4,7 +4,7 @@ import style from './index.module.css'
 const placeholder =
   'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
 
-export const UserHeader = ({ state: { user } }) => (
+export const UserHeader = ({ user = {} }) => (
   <header className={style.component}>
     <img src={user.avatarURL || placeholder} alt={user.name} />
     <div>
