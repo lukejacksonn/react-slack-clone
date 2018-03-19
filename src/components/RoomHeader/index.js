4ae5501f8198cd80333f8a55d2f815515b60ca43
Luke Jackson <lukejacksonn@gmail.com>
2018-03-19 10:45:52 +0000

    Refactors RoomHeader component

--- src/components/RoomHeader/index.js
+++ src/components/RoomHeader/index.js
@@ -1,32 +1,24 @@
 import React from 'react'
 import style from './index.module.css'
-import { UserList } from '../UserList'
 
 export const RoomHeader = ({
   state: { room, sidebar, user, userList },
-  actions: { setSidebar, createConvo, runCommand, setUserList },
-}) =>
-  room.id ? (
-    <header className={style.component}>
-      <button onClick={e => setSidebar(!sidebar)}>
-        <svg>
-          <use xlinkHref="index.svg#menu" />
-        </svg>
-      </button>
-      <h1>{room.name.replace(user.id, '')}</h1>
+  actions: { setSidebar, setUserList, createConvo, removeUserFromRoom },
+}) => (
+  <header className={style.component}>
+    <button onClick={e => setSidebar(!sidebar)}>
+      <svg>
+        <use xlinkHref="index.svg#menu" />
+      </svg>
+    </button>
+    <h1>{room.name.replace(user.id, '')}</h1>
+    {room.userIds.length > 2 ? (
       <div onClick={e => setUserList(!userList)}>
         <h4>{room.userIds.length}</h4>
         <svg>
           <use xlinkHref="index.svg#members" />
         </svg>
       </div>
-      {userList ? (
-        <UserList
-          room={room}
-          user={user}
-          createConvo={createConvo}
-          runCommand={runCommand}
-        />
-      ) : null}
-    </header>
-  ) : null
+    ) : null}
+  </header>
+)
