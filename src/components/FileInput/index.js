c8ed4a7df63245d112a04777017509e5a3019bbb
Luke Jackson <lukejacksonn@gmail.com>
2018-04-06 12:38:49 +0100

    Attachment format file (#51)
    * Use formatted file name as attachmant name
    
    * Adds download link for attachments of type file

--- src/components/FileInput/index.js
+++ src/components/FileInput/index.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import style from './index.module.css'
 
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = (Math.random() * 16) | 0,
-      v = c === 'x' ? r : (r & 0x3) | 0x8
-    return v.toString(16)
-  })
-}
-
 export const FileInput = ({ state: { user, message, room } }) =>
   room.id ? (
     <button>
@@ -25,7 +17,7 @@ export const FileInput = ({ state: { user, message, room } }) =>
               text: message || file.name,
               roomId: room.id,
               attachment: {
-                name: uuidv4(),
+                name: file.name.replace(/[^A-Za-z0-9._-]/g, '--'),
                 file,
               },
             })
